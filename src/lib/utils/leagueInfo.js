6f127d706e02b9b95dda78d72987745d021e9d33
Schlatter220 <coryschlatter@yahoo.com>
2026-01-01 18:08:30 -0800

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,9 +1,9 @@
 /*   STEP 1   */
 export const leagueID = "1259289618780540928"; // your league ID
 export const leagueName = "OG DADS FFL"; // your league name
-export const dues = 100; // (optional) used in template constitution page
+export const dues = 125; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
-export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
+export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
 export const homepageText = `
